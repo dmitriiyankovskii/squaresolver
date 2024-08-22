ace0e29ada513bd38c5db8790a4718b3145c34ce
dmitriiyankovskii <iankovskii.ds@phystech.edu>
2024-08-22 22:04:12 +0300

    Removed unused files

--- Solve.h
+++ Solve.h
@@ -1,7 +1,10 @@
+#ifndef SOLVE_H
+#define SOLVE_H
 
 const int SOLVING_ERROR = -2;
 
-
-int Solve_line(double b, double c, double *root1, double *root2);
-int Solve(double , double , double , double *, double *);
+int solve_line(double b, double c, double *root1, double *root2);
+int solve(double , double , double , double *, double *);
 int comp_D(double a, double b, double c, double *root1, double *root2);
+
+#endif
