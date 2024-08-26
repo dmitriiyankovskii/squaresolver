9288dfc56fbdc9d25c2e4d06de3ed7600079ec13
dmitriiyankovskii <iankovskii.ds@phystech.edu>
2024-08-26 15:35:53 +0300

    added bash scripts

---
+++ vari.sh
@@ -0,0 +1,8 @@
+DED_FLAGS="-Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wno-missing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -Werror=vla -D_DEBUG -D_EJUDGE_CLIENT_SIDE"
+
+FILE="vari.cpp"
+NAME_PROGRAMM="quad"
+echo "Start compilation"
+g++ $FILE -c -o vari.o $DED_FLAGS
+g++ -lm vari.o -o $NAME_PROGRAMM 
+echo "End compilation"
